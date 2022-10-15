55a1e05dcb34a09d335135aeeaf541a82c1878c5
xyz <xyz961014@gmail.com>
2022-10-15 11:20:31 +0200

    template bug fix

--- SSSB/SSSB/settings.py
+++ SSSB/SSSB/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SSSB.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "SSSB/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
