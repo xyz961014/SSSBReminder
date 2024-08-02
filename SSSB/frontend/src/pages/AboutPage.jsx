7537c5d82dc4a1165e958d5d04686aa1b2bb1597
xyz <xyz961014@gmail.com>
2024-08-03 00:00:40 +0200

    page router and title

---
+++ SSSB/frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>About Us</title>
+      </Helmet>
+      <h1>About Us</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
